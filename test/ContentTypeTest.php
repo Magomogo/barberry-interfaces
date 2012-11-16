781f24755cb4c9091c83a603848111920dae7ae7
Yulia Kovaleva <ykovaleva@xiag.ch>
2012-11-16 17:10:19 +0700

    Divided content types to: video, image, document and text. Added detection of file content whether it is video, image, etc.

--- test/ContentTypeTest.php
+++ test/ContentTypeTest.php
@@ -1,27 +1,46 @@
 <?php
 namespace Barberry;
 
-class ContentTypeTest extends \PHPUnit_Framework_TestCase {
-
-    public function testIsJpegCreatedByExtension() {
+class ContentTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsJpegCreatedByExtension()
+    {
         $this->assertEquals(
             'jpg',
             ContentType::byExtention('jpg')->standartExtention()
         );
     }
 
-    public function testContentTypeHasStandardExtension() {
+    public function testContentTypeHasStandardExtension()
+    {
         ContentType::byExtention('jpg')->standartExtention();
     }
 
-    public function testIsPhpCreatedByContentTypeString() {
+    public function testIsPhpCreatedByContentTypeString()
+    {
         $this->assertEquals(
             'php',
             ContentType::byString(file_get_contents(__FILE__))->standartExtention()
         );
     }
 
-    public function testMagicallyBecomesAString() {
+    public function testMagicallyBecomesAString()
+    {
         $this->assertEquals('image/jpeg', strval(ContentType::jpeg()));
+    }
+
+    public function testContentIsImage()
+    {
+        $this->assertTrue(ContentType::isImage(file_get_contents(__DIR__ . '/data/1x1.gif')));
+    }
+
+    public function testContentIsDocument()
+    {
+        $this->assertTrue(ContentType::isDocument(file_get_contents(__DIR__ . '/data/sample.pdf')));
+    }
+
+    public function testContentIsVideo()
+    {
+        $this->assertTrue(ContentType::isVideo(file_get_contents(__DIR__ . '/data/video.flv')));
     }
 }
